a7f7fccc961523b1754d9501d932d67555d22198
Breanna Nelson <breanna_a_nelson@hotmail.com>
2018-02-04 11:40:46 -0800

    Added web interface for hangman

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,45 @@
 
-// array of names
-var names = ["apple", "banana", "pear"];
+const PHOTO_PATH = "./assets/images/";
+const AUDIO_PATH = "./assets/audio/";
+
+// array of artistes info
+var artist = [
+    {
+        name: "adele",
+        photo: PHOTO_PATH + "adele-photo.jpg",
+        audio: AUDIO_PATH + "adele-audio.mp3"
+    },
+    {
+        name: "beyonce",
+        photo: PHOTO_PATH + "beyonce-photo.jpg",
+        audio: AUDIO_PATH + "beyonce-audio.mp3"
+    },
+    {
+        name: "drake",
+        photo: PHOTO_PATH + "drake-photo.jpg",
+        audio: AUDIO_PATH + "drake-audio.mp3"
+    }
+];
+
+var currentArtist = {
+    name: "",
+    photo: PHOTO_PATH + "default-photo.jpg",
+    audio: AUDIO_PATH + "default-audio.mp3"
+};
+
+// Gets Link for Theme Song
+var audioElement = document.createElement("audio");
+audioElement.setAttribute("src", currentArtist.audio);
+audioElement.play();
+
+
+
+
 // will be used to increment wins
 var wins = 0;
 // will be used to increment losses
 var losses = 0;
-// creating an array to store all wrong letters entered
+// initializing an array to store all wrong letters entered
 var wrongLetter = [];
 // var correctLetter = "";
 // will be used to increment the number of correct letters selected
@@ -13,36 +47,52 @@ var correctLetterCount = 0;
 var correctLetter = [];
 // will be used to increment the number of guesses
 var numGuessesRemaining = 12;
-
+// initializing var to store undersores
 var underlineWord = "";
-
+// initializing user entered keystroke
 var letter = "";
-
-
-
-
-
-
 
 
 // function returns a random num that will be used for the index of the array names
 function selectName() {
-    return Math.floor(Math.random() * names.length);
+    return Math.floor(Math.random() * artist.length);
 }
 
 // function will output to the screen the updated stats
 function reWriteStats() {
     var stats =
-        "<p>Wins<br>" + wins + "</p>" +
-        "<p>User Guess: " + letter + "</p>" +
-        "<p>Correct Letters: " + correctLetter + "</p>" +
-        "<p>Letters Already Guessed<br>" + wrongLetter + "</p>" +
-        "<p>Number of Guesses Remaining<br>" + numGuessesRemaining + "</p>" +
-        "<p>Current Word<br>" + underlineWord + "</p>" +
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Letters Already Guessed: " + wrongLetter + "</p>" +
+        "<p>Number of Guesses Remaining: " + numGuessesRemaining + "</p>" +
+        "<p>Current Word:<br>" + underlineWord + "</p>" +
         "<p>Losses : " + losses + "</p>";
 
     document.getElementById('gameStats').innerHTML = stats;
 }
+
+function displayName() {
+    document.getElementById('singerName').innerHTML = artist[index].name;
+}
+
+function updateAfterWin (ref) {
+    currentAritist = artist[ref];
+    audioElement.setAttribute("src", currentAritist.audio);
+    audioElement.play();
+    document.getElementById("singerPhoto").setAttribute("src", currentAritist.photo);
+}
+
+
+
+
+// Theme Button
+$(".play-button").on("click", function () {
+    audioElement.play();
+});
+
+$(".pause-button").on("click", function () {
+    audioElement.pause();
+});
+
 
 // function will reset all stats (besides wins and losses) and output to the screen
 function resetStats() {
@@ -52,11 +102,11 @@ function resetStats() {
     numGuessesRemaining = 12;
     letter = "";
     index = selectName();
-    console.log(names[index]);
-    underlineWord = generateUnderline(names[index]);
+    underlineWord = generateUnderline(artist[index].name);
     reWriteStats();
 }
 
+// generates underline
 function generateUnderline(str) {
     var results = "";
     for (var i = 0; i < str.length; i++) {
@@ -65,20 +115,20 @@ function generateUnderline(str) {
     return results;
 }
 
+// will replace chosen character in a string
 function replaceAt(string, index, replace) {
     return string.substring(0, index) + replace + string.substring(index + 1);
 }
 
 // function call for selectName(). Returns a num that will be used as the index of the array names
-
 var index = selectName();
-console.log(names[index])
-console.log(names[index].length);
-console.log("==> " + generateUnderline(names[index]));
 
-underlineWord = generateUnderline(names[index]);
+// calling underline generater
+underlineWord = generateUnderline(artist[index].name);
 
+// prints stats
 reWriteStats();
+
 
 document.onkeyup = function (event) {
     letter = event.key;
@@ -88,29 +138,27 @@ document.onkeyup = function (event) {
     if (type >= 65 && type <= 90) {
         if (!wrongLetter.includes(letter) && !correctLetter.includes(letter)) {
 
-            for (var i = 0; i < names[index].length; i++) {
-                if (names[index][i] === letter) {
+            for (var i = 0; i < artist[index].name.length; i++) {
+                if (artist[index].name[i] === letter) {
                     correctLetter.push(letter);
                     correctLetterCount++;
-                    console.log("Correct Letter: " + correctLetter);
-                    console.log("Correct Letter Count: " + correctLetterCount);
                     underlineWord = replaceAt(underlineWord, i * 2, letter);
-                    //GetElementBy
                 }
-                //GetElementByID
             }
 
             if (!correctLetter.includes(letter)) {
                 wrongLetter.push(letter);
-                console.log("Wrong Letter: " + wrongLetter);
                 numGuessesRemaining--;
             }
+
             reWriteStats();
-            // reWriteStats();
-            if (correctLetterCount === names[index].length) {
+
+            if (correctLetterCount === artist[index].name.length) {
                 wins++;
-                alert("You have won!");
-                resetStats()
+                displayName();
+                updateAfterWin(index);
+                resetStats();
+                // alert("You have won!");
             }
             if (wrongLetter.length === 12) {
                 alert("You have lost!");
@@ -125,7 +173,7 @@ document.onkeyup = function (event) {
         }
     }
     else {
-        alert("You did not enter a letter key!");
+        // alert("You did not enter a letter key!");
     }
 
 }
