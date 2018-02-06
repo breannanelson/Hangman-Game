0332c1c88543d3f38686897d50aaaac9c7e9473a
Breanna Nelson <breanna_a_nelson@hotmail.com>
2018-02-05 20:57:02 -0800

    Finalized js file. Updated comments and removed all console.logs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,9 @@
 
+// Paths to folders for images and audio
 const PHOTO_PATH = "./assets/images/";
 const AUDIO_PATH = "./assets/audio/";
 
-// array of artistes info
+// Array of artists info
 var artist = [
     {
         name: "adele",
@@ -76,17 +77,12 @@ var artist = [
     }
 ];
 
+// Defining/initializing default music and audio
 var currentArtist = {
     name: "",
     photo: PHOTO_PATH + "default-photo.jpg",
     audio: AUDIO_PATH + "default-audio.mp3"
 };
-
-// Gets Link for Song
-var audioElement = document.createElement("audio");
-audioElement.setAttribute("src", currentArtist.audio);
-audioElement.play();
-
 
 // will be used to increment wins
 var wins = 0;
@@ -94,9 +90,9 @@ var wins = 0;
 var losses = 0;
 // initializing an array to store all wrong letters entered
 var wrongLetter = [];
-// var correctLetter = "";
 // will be used to increment the number of correct letters selected
 var correctLetterCount = 0;
+// initializing an array to store all correct letters entered
 var correctLetter = [];
 // will be used to increment the number of guesses
 var numGuessesRemaining = 12;
@@ -105,13 +101,18 @@ var underlineWord = "";
 // initializing user entered keystroke
 var letter = "";
 
+// Gets Link for Song
+var audioElement = document.createElement("audio");
+audioElement.setAttribute("src", currentArtist.audio);
+// Plays default music when page is loaded
+audioElement.play();
 
-// function returns a random num that will be used for the index of the array names
+// function returns a random num that will be used as the index of the array of objects
 function selectName() {
     return Math.floor(Math.random() * artist.length);
 }
 
-// function will output to the screen the updated stats
+// function will output updated stats to the screen
 function reWriteStats() {
     var stats =
         "<p>Name an artist:<br>" + underlineWord + "</p>" +
@@ -123,14 +124,17 @@ function reWriteStats() {
     document.getElementById('gameStats').innerHTML = stats;
 }
 
+// Capitalizes first character of artist's name.
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+// Will display the artist's name
 function displayName() {
     document.getElementById('singerName').innerHTML = capitalizeFirstLetter(artist[index].name);
 }
 
+// Will update the current song and photo with the new
 function updateAfterWin(ref) {
     currentAritist = artist[ref];
     audioElement.setAttribute("src", currentAritist.audio);
@@ -139,7 +143,7 @@ function updateAfterWin(ref) {
 }
 
 
-// Song Button
+// Song Buttons
 $(".play-button").on("click", function () {
     audioElement.play();
 });
@@ -170,7 +174,8 @@ function generateUnderline(str) {
     return results;
 }
 
-// will replace chosen character in a string
+// will replace chosen character in a string.
+// changes "_" to the correct character
 function replaceAt(string, index, replace) {
     return string.substring(0, index) + replace + string.substring(index + 1);
 }
@@ -183,16 +188,19 @@ underlineWord = generateUnderline(artist[index].name);
 
 // prints stats
 reWriteStats();
-console.log(artist[index].name);
 
+// function that runs when a key is entered on the keyboard
 document.onkeyup = function (event) {
+    // stores the key entered by user
     letter = event.key;
     var type = event.keyCode;
 
-
+    // checks if key entered is a letter key
     if (type >= 65 && type <= 90) {
+        // if the letter key has not be entered before, the logic below will run. Otherwise, it will expect a different input.
         if (!wrongLetter.includes(letter) && !correctLetter.includes(letter)) {
 
+            // checks if the letter entered by the user is in the selected word. If it is, the correct letter is added to the "correctLetter" array, correct letter count increases by 1 and the letter will replace the displayed "_"
             for (var i = 0; i < artist[index].name.length; i++) {
                 if (artist[index].name[i] === letter) {
                     correctLetter.push(letter);
@@ -201,36 +209,37 @@ document.onkeyup = function (event) {
                 }
             }
 
+            // adds the wring letter to the array "wrongLetter"
             if (!correctLetter.includes(letter)) {
                 wrongLetter.push(letter);
                 numGuessesRemaining--;
             }
 
+            // updates stats
             reWriteStats();
 
+            // if all correct letters are guessed, 1 is added to wins, the name of the artist is displayed and their song played. It will also reset the stats
             if (correctLetterCount === artist[index].name.length) {
                 wins++;
                 displayName();
                 updateAfterWin(index);
                 resetStats();
-                console.log(artist[index].name);
-                // alert("You have won!");
             }
+            // if 12 wrong guesses are entered, will alert the user, add 1 to losses and update stats.
             if (wrongLetter.length === 12) {
                 alert("You have lost!");
                 losses++;
-                console.log(artist[index].name);
                 resetStats()
 
             }
 
         }
         else {
+            // alerts the user to enter a different letter if that letter has already been entered.
             alert("Try a different letter key!")
         }
     }
     else {
-        // alert("You did not enter a letter key!");
     }
 
 }
